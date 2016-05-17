e4a8457bcb1b59343950a5675a973df4c8e06aa0
quincy <1367811357@qq.com>
2016-05-17 10:50:46 +0800

    complete weather query and show

---
+++ app/src/main/java/com/ustc/hardy/coolweather/activity/WeatherActivity.java
@@ -0,0 +1,132 @@
+package com.ustc.hardy.coolweather.activity;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.ustc.hardy.coolweather.R;
+import com.ustc.hardy.coolweather.util.HttpUtil;
+import com.ustc.hardy.coolweather.util.Utility;
+
+
+/**
+ * Author: Created by QuincyHu on 2016/5/17 0017 09:45.
+ * Email:  zhihuqunxing@163.com
+ */
+public class WeatherActivity extends Activity {
+    private LinearLayout weatherInfoLayout;
+    //用于显示城市名
+    private TextView cityNameText;
+    //用于显示发布时间
+    private TextView publishText;
+    //用于显示天气描述信息
+    private TextView weatherDespText;
+    //用于显示气温1
+    private TextView temp1Text;
+    //用于显示气温2
+    private TextView temp2Text;
+    //用于显示当前日期
+    private TextView currentDataText;
+
+    private final String TAG = "WeatherActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.weather_layout);
+
+        //初始化各控件
+        weatherInfoLayout = (LinearLayout) findViewById(R.id.weather_info_layout);
+        cityNameText = (TextView) findViewById(R.id.city_name);
+        publishText = (TextView) findViewById(R.id.publish_text);
+        weatherDespText = (TextView) findViewById(R.id.weather_desp);
+        temp1Text = (TextView) findViewById(R.id.temp1);
+        temp2Text = (TextView) findViewById(R.id.temp2);
+        currentDataText = (TextView) findViewById(R.id.current_date);
+
+        //获取县级代号
+        String countryCode = getIntent().getStringExtra("country_code");
+        if(!TextUtils.isEmpty(countryCode)){
+            //县级代号非空时去查询天气
+            publishText.setText("查询中...");
+            weatherInfoLayout.setVisibility(View.INVISIBLE);
+            cityNameText.setVisibility(View.INVISIBLE);
+            queryWeatherCode(countryCode);
+        }else{
+            //没有县级代号时，直接显示本地天气
+            showWeather();
+        }
+    }
+
+    //查询县级代号对应的天气代号
+    private void queryWeatherCode(String countryCode) {
+        String address = "http://www.weather.com.cn/data/list3/city"+countryCode+".xml";
+        queryFromServer(address,"countryCode");
+    }
+
+    //查询天气代号所对应的天气
+    private void queryWeatherInfo(String weatherCode){
+        String address = "http://www.weather.com.cn/data/cityinfo/"+weatherCode+".html";
+        queryFromServer(address,"weatherCode");
+    }
+
+    //根据传入的地址和类型向服务器查询天气代号或天气信息
+    private void queryFromServer(final String address, final String type) {
+
+        HttpUtil.sendHttpRequest(address, new HttpUtil.HttpCallbackListener() {
+            @Override
+            public void onFinish(String response) {
+                if("countryCode".equals(type)){
+                    if(!TextUtils.isEmpty(response)){
+                        //从返回的数据中解析出天气代号
+                        String[] array = response.split("\\|");
+                        if(array != null && array.length == 2){
+                            String weatherCode = array[1];
+                            Log.d(TAG, "onFinish: 查询天气代号：" +weatherCode);
+                            queryWeatherInfo(weatherCode);
+                        }
+                    }
+                }else if("weatherCode".equals(type)){
+                    //处理服务器返回的天气信息
+                    Utility.handleWeatherResponse(WeatherActivity.this, response);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            showWeather();
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onError(Exception e) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        publishText.setText("同步失败");
+                    }
+                });
+            }
+        });
+    }
+
+    //从SharedPreference文件中读取存储的天气信息，并显示到界面上
+    private void showWeather() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        cityNameText.setText(prefs.getString("city_name", ""));
+        temp1Text.setText(prefs.getString("temp1",""));
+        temp2Text.setText(prefs.getString("temp2", ""));
+        weatherDespText.setText(prefs.getString("weather_desp", ""));
+        publishText.setText("今天"+prefs.getString("publish_time","") + "发布");
+        currentDataText.setText(prefs.getString("current_data", ""));
+        weatherInfoLayout.setVisibility(View.VISIBLE);
+        cityNameText.setVisibility(View.VISIBLE);
+    }
+}
